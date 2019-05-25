d070618be1176c12c7eb3e4ae1e88b0e8e834a6f
AnangM <social.anangmb@gmail.com>
2019-05-25 11:11:03 +0700

    fix typo

--- koneksi.php
+++ koneksi.php
@@ -1,12 +1,11 @@
 <?php
-$serveername ='localhost'
-$username = 'root';
-$password = '';
-$dbname = uasweb;
+$serveername ='localhost';
+$username = 'aoki';
+$password = 'anangaja';
+$dbname = 'uasweb';
 $conn = new mysqli($db_host,$db_usn,$db_pwd,$db_name);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 } 
-
 echo "Connected successfully";
 ?>
