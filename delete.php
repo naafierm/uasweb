91c3c3ca298b5b8aec477b15e9a7c8b570904d5e
AnangM <social.anangmb@gmail.com>
2019-05-25 14:11:35 +0700

    kelepasan hehehe

--- delete.php
+++ delete.php
@@ -1,11 +1,5 @@
 <?php
 require('koneksi.php');
 $id = $_POST['id'];
-$sql = "delete from prestasi where id='". $id."'";
-if($conn->query($sql)===TRUE){
-    header("Location: index.php");
-    exit();
-}else{
-    echo "Error menghapus data : " . $conn->error;
-}
+
 $conn->close();
